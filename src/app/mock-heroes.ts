70c9394effc61870ad85dc6a0fe0d88690a080eb
David <84280447+DavidMelloMiura@users.noreply.github.com>
2021-07-30 16:28:33 -0300

    tour-of-heroes-Adicionar-servicos

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,13 +1,13 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  { id: 11, name: 'Dr Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
+  { id: 11, name: 'Mexilhão ' },
+  { id: 12, name: 'Mexilhãozinho' },
+  { id: 13, name: 'Tony FULL STARK' },
   { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
+  { id: 15, name: 'Super VS' },
+  { id: 16, name: 'W3Scholl' },
+  { id: 17, name: 'Stack Overflow' },
   { id: 18, name: 'Dr IQ' },
   { id: 19, name: 'Magma' },
   { id: 20, name: 'Tornado' }
